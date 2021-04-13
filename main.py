79ffc337cfa48297710d8d8003b8767c6ffc9a76
Pranjal Patra <pranjal.cna@gmail.com>
2021-04-13 09:27:57 -0230

    add title and latte button

--- main.py
+++ main.py
@@ -1,13 +1,25 @@
 import tkinter as tk
 from tkinter import ttk
 
+
+def button_latte_click_handler():
+    print('I want some coffee')
+
+
+# Step 1: Create Root Window
 root = tk.Tk()
 root.title('Coffee Machine')
 root.geometry('400x300')
 
+# Step 2: Create Frame
 frame_coffee = ttk.Frame(root)
 frame_coffee.pack(fill=tk.BOTH, expand=True)
 
+# Step 3: Start Adding UI Elements
+label_title = ttk.Label(frame_coffee, text='Coffee Machine')
+label_title.grid(columnspan=3, row=0)
 
+button_latte = ttk.Button(frame_coffee, text='Latte', command=button_latte_click_handler)
+button_latte.grid(column=0, row=1)
 
 root.mainloop()
